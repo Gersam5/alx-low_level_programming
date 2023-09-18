70acad38a1c20a8eca4083475c069c1c43667232
Gersam5 <gersamsemu5@gmail.com>
2023-09-18 07:31:08 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,25 @@
-#include "main.h"
-
-/**
- * puts_half - prints half of a string.
- * @str: input string.
- * Return: no return.
- */
-void puts_half(char *str)
-{
-	int count = 0, i;
-
-	while (count >= 0)
-	{
-		if (str[count] == '\0')
-			break;
-		count++;
-	}
-	if (count % 2 == 1)
-		i = count / 2;
-	else
-		i = (count - 1) / 2;
-
-	for (i++; i < count; i++)
-		_putchar(str[i]);
- _putchar('\n');
-}
+#include "main.h"                                                  
+#include <stdio.h>                                                 
+                                                                   
+/**                                                                
+ * puts_half - Prints half of a string.                            
+ * @str: The string to be printed.                                 
+ */                                                                
+void puts_half(char *str)                                          
+{                                                                  
+        int index = 0, len = 0, n;                                 
+                                                                   
+        while (str[index++])                                       
+                len++;                                             
+                                                                   
+        if ((len % 2) == 0)                                        
+                n = len / 2;                                       
+                                                                   
+        else                                                       
+                n = (len + 1) / 2;                                 
+                                                                   
+        for (index = n; index < len; index++)                      
+                putchar(str[index]);                               
+                                                                   
+        putchar('\n');                                             
+} 
