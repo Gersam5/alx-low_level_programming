ec53f5fc640074cc95d018fb58b37f918f262e3f
Gersam5 <gersamsemu5@gmail.com>
2023-10-23 22:09:26 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-main.c
+++ 0x13-more_singly_linked_lists/0-main.c
@@ -1,32 +1,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include "lists.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *next;
+    struct dlistint_s *prev;
+} dlistint_t;
+
+void add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+
+    new = malloc(sizeof(dlistint_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return;
+    }
+    new->n = n;
+    new->next = NULL;
+    new->prev = *head;
+    if (*head != NULL)
+        (*head)->next = new;
+    *head = new;
+}
+
+int sum_dlistint(dlistint_t *head)
+{
+    int sum = 0;
+
+    while (head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+    return (sum);
+}
+
 int main(void)
 {
-listint_t *head;
-listint_t *new;
-listint_t hello = {8, NULL};
-size_t n;
+    dlistint_t *head;
+    size_t n;
 
-head = &hello;
-new = malloc(sizeof(listint_t));
-if (new == NULL)
-{
-printf("Error\n");
-return (1);
-}
-new->n = 9;
-new->next = head;
-head = new;
-n = print_listint(head);
-printf("-> %lu elements\n", n);
-free(new);
-return (0);
+    head = NULL;
+    add_dnodeint_end(&head, 8);
+    add_dnodeint_end(&head, 9);
+    n = sum_dlistint(head);
+    printf("-> %lu elements\n", n);
+    return (0);
 }
